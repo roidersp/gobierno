14c305bd067df6941f6c4650070b1a235bae1840
Rodrigo Luis Santander <roidersp@gmail.com>
2014-12-18 10:54:41 -0600

    gfdf
    gdfdfg

--- js/indepth.js
+++ js/indepth.js
@@ -1,6 +1,6 @@
 var disqus_shortname = 'juanfutbol';
 var disqus_identifier;
-var disqus_url="74abbcfc-0d83-4800-8591-2a675ca92124";
+var disqus_url="becebde4-0d5b-4345-b62b-403c38d81082";
 var disqus_number_c=2;
 var disqus_per_page=3;
 var tamaño_total=1920;
